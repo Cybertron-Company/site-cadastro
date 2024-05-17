7117770dc18b671ebef8455d1d25ba12067b2e9a
cybertron_company <148670338+Cybertron-Company@users.noreply.github.com>
2024-05-17 01:38:40 -0300

    teste origin 2.0

--- src/main/java/com/cybertroncompany/sitecadastro/security/SecurityConfig.java
+++ src/main/java/com/cybertroncompany/sitecadastro/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.cybertroncompany.sitecadastro.security;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
