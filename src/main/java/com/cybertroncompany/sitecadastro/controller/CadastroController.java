e623d5f8ae9aec4e1ced641470f296007e235c50
cybertron_company <148670338+Cybertron-Company@users.noreply.github.com>
2024-05-25 11:14:32 -0300

    teste commit

--- src/main/java/com/cybertroncompany/sitecadastro/controller/CadastroController.java
+++ src/main/java/com/cybertroncompany/sitecadastro/controller/CadastroController.java
@@ -19,14 +19,14 @@ public class CadastroController {
     //@PreAuthorize("hasAnyRole('ADMIN')")
     //@CrossOrigin(origins = "https://cybertron-cadastro.up.railway.app/")
     @PostMapping("/usuario")
-    @CrossOrigin(origins = "http://localhost:3000/")
+    @CrossOrigin(origins = "https://cybertroncompany.com.br/")
     public ResponseEntity<RetornoDTO> cadastrarUsuario(@RequestBody ClienteDTO clienteDTO) {
         RetornoDTO retorno = clienteService.cadastrar(clienteDTO);
         return ResponseEntity.ok().body(retorno);
     }
 
     @GetMapping("/usuario")
-    @CrossOrigin(origins = "http://localhost:3000/")
+    @CrossOrigin(origins = "https://cybertroncompany.com.br/")
     public ResponseEntity<String> dados() {
         String retorno = clienteService.generateNewApiKey();
         return ResponseEntity.ok().body(retorno);
